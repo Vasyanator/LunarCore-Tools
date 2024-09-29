258a467e19351e64acd41d13571b62b3b9c4ff12
Vasyanator <135327897+Vasyanator@users.noreply.github.com>
2024-09-29 13:56:14 +0400

    Create stats_id.py

---
+++ stats_id.py
@@ -0,0 +1,48 @@
+substats = {
+    "HP" : 1,
+    "ATK" : 2,
+    "DEF" : 3,
+    "HP%" : 4,
+    "ATK%" : 5,
+    "DEF%" : 6,
+    "Speed" : 7,
+    "Crit rate" : 8,
+    "Crit dmg" : 9,
+    "Effect hit rate" : 10,
+    "Effect res" : 11,
+    "Break effect" : 12
+}
+stats_3 = {
+    "HP%" : 1,
+    "ATK%" : 2,
+    "DEF%" : 3,
+    "Crit rate" : 4,
+    "Crit dmg" : 5,
+    "Outgoing Healing" : 6,
+    "Effect hit rate" : 7,
+}
+stats_4 = {
+    "HP%" : 1,
+    "ATK%" : 2,
+    "DEF%" : 3,
+    "Speed" : 4
+}
+stats_5 = {
+    "HP%" : 1,
+    "ATK%" : 2,
+    "DEF%" : 3,
+    "Physical Damage" : 4,
+    "Fire Damage" : 5,
+    "Ice Damage" : 6,
+    "Lightning Damage" : 7,
+    "Wind Damage" : 8,
+    "Quantum Damage" : 9,
+    "Imaginary Damage" : 10,
+}
+stats_6 = {
+    "Break effect" : 1,
+    "Energy regeneration rate" : 2,
+    "HP%" : 3,
+    "ATK%" : 4,
+    "DEF%" : 5,
+}
