a465036556e83aaa94b2a3b711db67752bac12e3
Vasyanator <135327897+Vasyanator@users.noreply.github.com>
2024-11-10 17:30:20 +0400

    Update tab_opencommand.py

--- tab_opencommand.py
+++ tab_opencommand.py
@@ -4,8 +4,9 @@ import tkinter as tk
 from tkinter import ttk
 
 class OpenCommandTab:
-    def __init__(self, notebook):
+    def __init__(self, notebook, localization):
         self.frame = ttk.Frame(notebook)
+        self.localization = localization
         
-        self.label = tk.Label(self.frame, text="If you know Java, please implement something like github.com/jie65535/gc-opencommand-plugin, I will be grateful and implement its support here.", justify="left")
+        self.label = tk.Label(self.frame, text=self.localization["message"], justify="left")
         self.label.pack(fill="both", expand=True, padx=10, pady=10)
