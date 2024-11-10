200bb78965d59d8ab39cb5a6270ee31dfe15a987
Vasyanator <135327897+Vasyanator@users.noreply.github.com>
2024-11-10 17:32:02 +0400

    Create ru.py

---
+++ locales/ru.py
@@ -0,0 +1,149 @@
+# locales/ru.py
+
+main = {
+    'file_not_found_title': "Файл не найден",
+    'file_not_found_message': "Файл '{filename}' не найден. Пожалуйста выберите файл.",
+    'select_data_file_title': "Выбрать файл данных",
+    'text_files': "Текстовые файлы",
+    'all_files': "Все файлы",
+    'no_file_selected_title': "Файл не выбран",
+    'no_file_selected_message': "Файл не выбран. Выход...",
+    'file_error_title': "Ошибка файла",
+    'file_error_message': "Не получилось прочитать файл '{filename}'. Пожалуйста выберите другой файл.",
+    'invalid_file_title': "Неверный файл",
+    'invalid_file_message': " Выбранный файл '{filename}' не является Lunar Core Handbook. Пожалуйста, выберите другой файл.",
+    'window_title': "{program_name} {program_version} - With {handbook_version} Handbook",
+    'tab_relic_generation': "Создание реликвий",
+    'tab_items': "Предметы",
+    'tab_spawn': "Спавн",
+    'tab_mazes': "Локации",
+    'tab_avatars': "Персонажи",
+    'tab_commands': "Команды",
+    'tab_opencommand_plugin': "Плагин OpenCommand",
+    'autocopy_label': "Автокопирование",
+    'copy_button_label': "Копировать",
+    'copied_title': "Скопировано",
+    'command_copied_message': "Скопировано в буфер обмена.",
+    'no_command_title': "Нет команды",
+    'no_command_message': "Нет команды для копирования."
+}
+
+# Example localization for the Planars Tab
+planars_tab = {
+    'Select_Type': 'Выберите тип',
+    'Default': 'Пещерные',
+    'Planars': 'Планарные',
+    'Select_Rarity:': 'Редкость:',
+    'Search:': 'Поиск:',
+    'Main_Stat:': 'Основная характеристика:',
+    'Select_Level:': 'Уровень:',
+    'Additional_stats:': 'Дополнительные характеристики:',
+    'Select_stat:': 'Выбрать стат:',
+    'Select_Value:': 'Выбрать значение:',
+    'Quantity:': 'Количество:',
+    'Level:': 'Уровень:',
+    'Add': 'Добавить',
+    'Current_additional_stats:': 'Текущие дополнительные характеристики:',
+    'Remove_Selected': 'Убрать выбранную',
+    'Clear_All': 'Убрать все',
+    'Include_-maxsteps': 'Включить -maxsteps',
+    'Levels_of_additional_stats': 'Включить уровни доп.характеристик (только на HotaruSR)',
+    'Main_Stat:_Fixed': 'Основная характеристика: фиксированно',
+    'Select_Main_Stat:': 'Выбрать основную характеристику:',
+    'No_Main_Stats_Available': 'Нет основных характеристик', 
+    'Select_an_item_first' : 'Сначала выберите предмет',
+    'Stats': {
+        'HP': 'HP',
+        'ATK': 'Атака',
+        'DEF': 'Защита',
+        'HP%': 'HP%',
+        'ATK%': 'Атака%',
+        'DEF%': 'Защита%',
+        'Speed': 'Скорость',
+        'Crit rate': 'Крит. шанс',
+        'Crit dmg': 'Крит. урон',
+        'Effect hit rate': 'Шанс попадания эффектов',
+        'Effect res': 'Сопротивление эффектам',
+        'Break effect': 'Эффект пробития',
+        "Outgoing Healing" : "Исходящее исцеление",
+        "Energy regeneration rate" : "Скорость восстановления энергии",
+        "Physical Damage" : "Бонус Физ урона",
+        "Fire Damage" : "Бонус Огненного урона",
+        "Ice Damage" : "Бонус Ледяного урона",
+        "Lightning Damage" : "Бонус Электрического урона",
+        "Wind Damage" : "Бонус Ветряного урона",
+        "Quantum Damage" : "Бонус Квантового урона",
+        "Imaginary Damage" : "Бонус Мнимого урона",
+    },
+}
+
+# Similarly, add dictionaries for other tabs:
+items_tab = {
+    "Search": "Поиск",
+    "Quantity": "Количество:",
+    'Base Materials': "Базовые материалы",
+    'Lightcones' : "Световые конусы",
+    'Materials': "Материалы",
+    'Other': "Другое",
+    'Unknown': "Неизвестные",
+    'Level': "Уровень",
+    'Rank': "Ранг",
+}
+
+spawn_tab = {
+    "Search": "Поиск",
+    "Search_NPC_Monsters": "Поиск NPC монстров:",
+    "Level": "Уровень:",
+    "Number": "Номер:",
+    "Radius": "Радиус:",
+    "Select": "Выбрать:",
+    "Search_Battle_Monsters": "Поиск боевых монстров:",
+    "Add": "Добавить",
+    "Selected_Battle_Monsters": "Выбранные боевые монстры:",
+    "Remove": "Убрать",
+    "Clear": "Очистить",
+    "For_world_level": "Для уровня мира:",
+    "Search_Battle_Stages": "Поиск готовых битв:",
+    "Props": "Объекты",
+    "Monsters": "Монстры",
+    "Custom_monsters": "Кастомные монстры",
+    "Stages" : "Волны",
+
+}
+
+mazes_tab = {
+    "Search": "Поиск:",
+}
+
+avatars_tab = {
+    "Lineup": "Отряд",
+    "USE_AT_YOUR_OWN_RISK": "ИСПОЛЬЗУЙТЕ НА СВОЙ СТРАХ И РИСК",
+    "Search": "Поиск:",
+    "Lineup_List": "Список отряда:",
+    "Add": "Добавить",
+    "Remove": "Убрать",
+    "Clear": "Очистить",
+    "Warning": "Предупреждение",
+    "Properties": "Свойства",
+    "Target": "Цель:",
+    "Level_(1_80)": "Уровень (1-80):",
+    "Ascension_(0_6)": "Возвышение (0-6):",
+    "Eidolon_(0_6)": "Эйдолон (0-6):",
+    "Skill_Levels_(1_12)": "Уровень навыков (1-12):",
+}
+
+commands_tab = {
+    "Start_Commands": "Стартовые команды",
+    "Base_Commands": "Базовые команды",
+    "Custom_Commands": "Кастомные команды",
+    "Name": "Имя:",
+    "Save": "Сохранить",
+    "Load": "Загрузить",
+    "Delete": "Удалить",
+    "Error": "Ошибка",
+    "Warning": "Предупреждение",
+}
+
+opencommand_tab = {
+    "message": "If you know Java, please implement something like github.com/jie65535/gc-opencommand-plugin, I will be grateful and implement its support here.",
+}
